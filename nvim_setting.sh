88ad535a7556cf9970a7bb6f9022af4790148b3c
silvern1990 <silvern1990@gmail.com>
2023-06-21 16:51:48 +0900

    nvim setting update

--- nvim_setting.sh
+++ nvim_setting.sh
@@ -35,16 +35,6 @@ fi
 
 cat > ~/.config/nvim/init.vim << "EOF"
 
-
-call plug#begin('~/.vim/plugged')
-
-Plug 'morhetz/gruvbox' "color
-Plug 'baines/vim-colorscheme-thaumaturge' "color
-Plug 'preservim/nerdtree' 
-Plug 'vim-airline/vim-airline'
-
-call plug#end()
-
 set ic
 set nu
 set softtabstop=4
@@ -54,7 +44,6 @@ set ruler
 set showmode
 set smartindent
 set splitright "locate new vsplit window at right panel
-set clipboard+=unnamedplus
 
 "this makes backspace key work properly in gvim on Windows
 "set backspace=2
@@ -65,11 +54,83 @@ set clipboard+=unnamedplus
 
 syntax on
 
-color gruvbox
 set background=dark
 
-map ;n <ESC>:NERDTreeToggle<CR>
-map ;t <ESC>:TlistToggle<CR>
+call plug#begin('~/.vim/plugged')
+
+Plug 'morhetz/gruvbox' "color
+Plug 'baines/vim-colorscheme-thaumaturge' "color
+Plug 'preservim/nerdtree' 
+Plug 'vim-airline/vim-airline'
+
+" -- python plugin start --
+Plug 'Shougo/ddc.vim'
+Plug 'vim-denops/denops.vim'
+Plug 'Shougo/ddc-matcher_head'
+Plug 'Shougo/ddc-sorter_rank'
+Plug 'Shougo/ddc-ui-native'
+Plug 'Shougo/ddc-source-around'
+" -- python plugin end --
+
+call plug#end()
+
+
+" Customize global settings
+
+" You must set the default ui.
+" NOTE: native ui
+" https://github.com/Shougo/ddc-ui-native
+call ddc#custom#patch_global('ui', 'native')
+
+" Use around source.
+" https://github.com/Shougo/ddc-source-around
+call ddc#custom#patch_global('sources', ['around'])
+
+" Use matcher_head and sorter_rank.
+" https://github.com/Shougo/ddc-matcher_head
+" https://github.com/Shougo/ddc-sorter_rank
+call ddc#custom#patch_global('sourceOptions', #{
+      \ _: #{
+      \   matchers: ['matcher_head'],
+      \   sorters: ['sorter_rank']},
+      \ })
+
+" Change source options
+call ddc#custom#patch_global('sourceOptions', #{
+      \   around: #{ mark: 'A' },
+      \ })
+call ddc#custom#patch_global('sourceParams', #{
+      \   around: #{ maxSize: 500 },
+      \ })
+
+" Customize settings on a filetype
+call ddc#custom#patch_filetype(['c', 'cpp'], 'sources',
+      \ ['around', 'clangd'])
+call ddc#custom#patch_filetype(['c', 'cpp'], 'sourceOptions', #{
+      \   clangd: #{ mark: 'C' },
+      \ })
+call ddc#custom#patch_filetype('markdown', 'sourceParams', #{
+      \   around: #{ maxSize: 100 },
+      \ })
+
+" Mappings
+
+" <TAB>: completion.
+inoremap <silent><expr> <TAB>
+\ pumvisible() ? '<C-n>' :
+\ (col('.') <= 1 <Bar><Bar> getline('.')[col('.') - 2] =~# '\s') ?
+\ '<TAB>' : ddc#map#manual_complete()
+
+" <S-TAB>: completion back.
+inoremap <expr><S-TAB>  pumvisible() ? '<C-p>' : '<C-h>'
+
+" Use ddc.
+call ddc#enable()
+
+
+" key map
+
+map <leader>t <ESC>:NERDTreeToggle<CR>
 
 EOF
 
