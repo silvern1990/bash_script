3df615552a48d99cb92b318922aa74e7a8419ccd
silvern1990 <silvern1990@gmail.com>
2025-02-05 00:35:21 +0900

    a

--- profile.sh
+++ profile.sh
@@ -1,3 +1,5 @@
 cat >> ~/.bash_profile << "EOF"
 alias ls='ls --color=auto --time-style=long-iso'
 EOF
+
+alias ssh-cmd='$(openssl enc -aes-256-cbc -d -in ~/.ssh/ssh_exp.enc -out /tmp/exp) && expect /tmp/exp'
