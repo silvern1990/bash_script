a633b9d539f168e3a93dad8b460b34ad93bc7b2f
Park-jin-su <kaca2100@ndps.co.kr>
2025-06-02 15:31:15 +0900

    test

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 # mako notify setting
-dnf -y install mako
+sudo dnf -y install mako
 
 mkdir -p ~/.config/mako
 
@@ -19,3 +19,116 @@ cat > ~/.config/foot/foot.ini << "EOF"
 font=Unifont:size=12
 EOF
 
+
+# fuzzel setting
+sudo dnf -y install fuzzel
+
+cat > ~/.config/fuzzel/fuzzel.ini << "EOF"
+[colors]
+background=282a36fa
+text-color=ffffff
+selection-color=3d4474fa
+border=fffffffa
+
+[border]
+border-width=2
+EOF
+
+
+# swayr & swayrbar setting
+if cargo; then
+else
+	sudo dnf -y install cargo
+fi
+
+git clone https://git.sr.ht/\~tsdh/swayr ~/swayr
+cd ~/swayr && cargo build
+cargo install swayr swayrbar
+
+cat > ~/.config/swayr/config.toml << "EOF"
+[menu]
+executable = "fuzzel"
+args = [
+	'--dmenu',
+	'--lines=5',
+	'--prompt=Select window: ',
+	'--width=50',
+]
+
+[format]
+window_format = '{app_name} - {title} - Workspace: {workspace_name}'
+
+[layout]
+auto_tile = false
+auto_tile_min_window_width_per_output_width = [
+    [
+    800,
+    400,
+],
+    [
+    1024,
+    500,
+],
+    [
+    1280,
+    600,
+],
+    [
+    1400,
+    680,
+],
+    [
+    1440,
+    700,
+],
+    [
+    1600,
+    780,
+],
+    [
+    1680,
+    780,
+],
+    [
+    1920,
+    920,
+],
+    [
+    2048,
+    980,
+],
+    [
+    2560,
+    1000,
+],
+    [
+    3440,
+    1200,
+],
+    [
+    3840,
+    1280,
+],
+    [
+    4096,
+    1400,
+],
+    [
+    4480,
+    1600,
+],
+    [
+    7680,
+    2400,
+],
+]
+
+[focus]
+lockin_delay = 750
+
+[misc]
+seq_inhibit = false
+
+[swaymsg_commands]
+include_predefined = true
+EOF
