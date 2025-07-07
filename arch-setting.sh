9aa806c40ae4205955408a83a5734cab1a572814
silvern1990 <silvern1990@gmail.com>
2025-07-07 10:58:34 +0900

    a

--- arch-setting.sh
+++ arch-setting.sh
@@ -30,15 +30,16 @@ exec_always swaybg -i ~/.config/sway/background/background.jpg -m fit
 
 exec ~/.config/sway/lid-handler.py
 
+output DP-1 pos 0 0
+output eDP-1 pos 640 1440
+
 client.focused          #4c7899 #28557788 #ffffff #2e9ef4
 client.unfocused        #333333 #22222277 #ffffff #292d3e
 client.urgent           #2f343a #900000 #ffffff #900000
 client.placeholder      #000000 #0c0c0c #ffffff #000000
 client.background       #ffffff
 
-
 bindsym $mod+Shift+M exec swaylock -c 000000
-
 EOF
 
     # sway lid event control
